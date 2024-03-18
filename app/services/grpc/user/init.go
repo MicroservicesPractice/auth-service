06b9aa07b19615f122c0443536729a5dffae262c
RomaZherko210 <r.zherko@invatechs.com>
2024-03-18 11:54:41 +0300

    refactor

--- app/services/grpc/user/init.go
+++ app/services/grpc/user/init.go
@@ -1,13 +1,18 @@
 package user
 
 import (
+	"auth-service/app/helpers"
 	"auth-service/app/helpers/log"
+	"fmt"
 
 	"google.golang.org/grpc"
 )
 
+var USER_SERVICE_HOST = helpers.GetEnv("USER_SERVICE_HOST")
+var USER_SERVICE_GRPC_PORT = helpers.GetEnv("USER_SERVICE_GRPC_PORT")
+
 func ConnectUserServiceGrpc() UserClient {
-	conn, err := grpc.Dial("localhost:6003", grpc.WithInsecure())
+	conn, err := grpc.Dial(fmt.Sprintf("%v:%v", USER_SERVICE_HOST, USER_SERVICE_GRPC_PORT), grpc.WithInsecure())
 	if err != nil {
 		log.GrpcLog(log.Error, "user-service", "can't connect to grpc service")
 	}
