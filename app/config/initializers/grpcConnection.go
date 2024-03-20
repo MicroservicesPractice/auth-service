3b84c2a38b312f1104e5133e90cfed13f974bea2
RomaZherko210 <r.zherko@invatechs.com>
2024-03-20 11:15:56 +0300

    refactor

---
+++ app/config/initializers/grpcConnection.go
@@ -0,0 +1,27 @@
+package initializers
+
+import (
+	"auth-service/app/helpers"
+	"auth-service/app/helpers/log"
+	"fmt"
+
+	grpcApi "github.com/MicroservicesPractice/grpc-api/generated/user"
+
+	"google.golang.org/grpc"
+)
+
+var USER_SERVICE_HOST = helpers.GetEnv("USER_SERVICE_HOST")
+var USER_SERVICE_GRPC_PORT = helpers.GetEnv("USER_SERVICE_GRPC_PORT")
+
+func ConnectUserServiceGrpc() grpcApi.UserClient {
+	conn, err := grpc.Dial(fmt.Sprintf("%v:%v", USER_SERVICE_HOST, USER_SERVICE_GRPC_PORT), grpc.WithInsecure())
+	if err != nil {
+		log.GrpcLog(log.Error, "user-service", "can't connect to grpc service")
+	}
+	// defer conn.Close()
+
+	client := grpcApi.NewUserClient(conn)
+
+	return client
+
+}
